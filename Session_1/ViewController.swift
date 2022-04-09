34d76c7024687b3e4d3b21f73602ca8a75ba8dc4
Sabina Azimova <sabinasazimova@gmail.com>
2022-04-09 15:41:27 +0400

    Initial Commit

---
+++ Session_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Session_1
+//
+//  Created by Sabina Azimova on 09.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
